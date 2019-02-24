86488042617d94d25a4a6011de16e3b92f9bd959
Stanislav Machel <stanislav.machel@gmail.com>
2019-02-24 19:57:22 +0200

    Command Object and Type conversion, Save Recipe from RecipeCommand

--- src/main/java/com/example/recipeapp/services/RecipeService.java
+++ src/main/java/com/example/recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.example.recipeapp.services;
 
+import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.domain.Recipe;
 
 import java.util.Set;
@@ -7,4 +8,5 @@ import java.util.Set;
 public interface RecipeService {
     Set<Recipe> getRecipes();
     Recipe findById(Long id);
+    RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
 }
--- src/main/java/com/example/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/example/recipeapp/services/RecipeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.recipeapp.services;
 
+import com.example.recipeapp.commands.RecipeCommand;
+import com.example.recipeapp.converters.RecipeCommandToRecipe;
+import com.example.recipeapp.converters.RecipeToRecipeCommand;
 import com.example.recipeapp.domain.Recipe;
 import com.example.recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -14,9 +18,14 @@ import java.util.Set;
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe,
+                             RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -35,5 +44,14 @@ public class RecipeServiceImpl implements RecipeService {
         }
 
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(recipeCommand);
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
