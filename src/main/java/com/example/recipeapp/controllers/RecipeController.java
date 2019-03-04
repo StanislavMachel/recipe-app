5b9157bc1e4c84820bbc766c15308f71925b12cc
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-04 20:41:25 +0200

    Delete Recipe - Get because without javascript

--- src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -2,10 +2,12 @@ package com.example.recipeapp.controllers;
 
 import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -16,6 +18,7 @@ public class RecipeController {
     }
 
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable Long id, Model model){
 
@@ -24,6 +27,7 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("/recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
@@ -38,9 +42,19 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeForm";
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+        log.debug("Deleted id:" + id);
+        recipeService.deleteById(Long.valueOf(id));
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/recipeapp/services/RecipeService.java
+++ src/main/java/com/example/recipeapp/services/RecipeService.java
@@ -10,4 +10,6 @@ public interface RecipeService {
     Recipe findById(Long id);
     RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
     RecipeCommand findCommandById(Long id);
+
+    void deleteById(Long idToDelete);
 }
