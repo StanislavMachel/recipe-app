a51b09da000dfc47492cbb5bb22668e964ff8af7
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-09 18:33:02 +0200

    View ingredients

--- src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -52,9 +52,8 @@ public class RecipeController {
     @GetMapping
     @RequestMapping("/recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
-        log.debug("Deleted id:" + id);
+        log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
-
         return "redirect:/";
     }
 }
