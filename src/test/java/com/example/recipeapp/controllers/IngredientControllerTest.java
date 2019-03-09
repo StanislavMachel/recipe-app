69fbe8c805c112a26af4bf6f999130e6dcf65ec7
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-09 19:05:42 +0200

    View ingredient

--- src/test/java/com/example/recipeapp/controllers/IngredientControllerTest.java
+++ src/test/java/com/example/recipeapp/controllers/IngredientControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.recipeapp.controllers;
 
+import com.example.recipeapp.commands.IngredientCommand;
 import com.example.recipeapp.commands.RecipeCommand;
+import com.example.recipeapp.services.IngredientService;
 import com.example.recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +22,8 @@ public class IngredientControllerTest {
 
     @Mock
     RecipeService recipeService;
+    @Mock
+    IngredientService ingredientService;
 
     IngredientController ingredientController;
 
@@ -28,7 +32,7 @@ public class IngredientControllerTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        ingredientController = new IngredientController(recipeService);
+        ingredientController = new IngredientController(recipeService, ingredientService);
         mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
     }
 
@@ -47,5 +51,20 @@ public class IngredientControllerTest {
 
         //then
         verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    public void testShowIngredient() throws Exception {
+        //given
+        IngredientCommand ingredientCommand = new IngredientCommand();
+
+        //when
+        when(ingredientService.findByRecipeIdAndIngredientId(anyLong(), anyLong())).thenReturn(ingredientCommand);
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredient/2/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/show"))
+                .andExpect(model().attributeExists("ingredient"));
     }
 }
