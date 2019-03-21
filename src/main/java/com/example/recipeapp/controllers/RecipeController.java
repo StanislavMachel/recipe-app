b5aacc7a70c91c9866795300dc36a62dc9d31e0e
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-21 21:25:47 +0200

    Create ImageController and change html in forms

--- src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -18,8 +18,8 @@ public class RecipeController {
     }
 
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show")
+
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable Long id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(id));
@@ -27,30 +27,29 @@ public class RecipeController {
         return "recipe/show";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/new")
+
+    @GetMapping("/recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeForm";
     }
 
-    @PostMapping
-    @RequestMapping("/recipe")
+
+    @PostMapping("/recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/update")
+
+    @GetMapping("/recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeForm";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/delete")
+    @GetMapping("/recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
         log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
